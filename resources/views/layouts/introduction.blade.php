95123e4d5f382c366f87c79f02fabf08c3eae9e1
Benson <a23248630@gmail.com>
2019-01-17 02:54:07 +0800

    day2 當義工

--- resources/views/layouts/introduction.blade.php
+++ resources/views/layouts/introduction.blade.php
@@ -1,11 +1,11 @@
 <!-- Introduction -->
-    <section id="two" class="wrapper withimg">
+    <section id="two" class="wrapper withimg introduce">
         <div class='box-1'>
             <header class="major">
                 <h2>松數營簡介</h2>
                 <p>Introduction</p>
             </header>
-            <p>我們是國立中央大學數學系第19屆松數營</p>
+            <p>我們是國立中央大學數學系第20屆松數營</p>
             <p>不論你是熱愛數學還是畏懼數學</p>
             <p>我們都歡迎你與我們一起</p>
             <p>在今年暑假 展開全新冒險！</p>
